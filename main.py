9c6dcc00c57f97fbc0fee1f67a055d0ed31f0a3d
Aryanpatel335 <aryanp862002@gmail.com>
2023-10-07 19:37:52 -0400

    strategy modified

--- main.py
+++ main.py
@@ -47,8 +47,8 @@ df_final['tqqq_intraday'] = tqqq_intraday
 df_final['sqqq_intraday'] = sqqq_intraday
 df_final['qqq_intraday'] = qqq_intraday
 
-
-# if adj close is between 0 - 1.5% or -1.5-0 we wiill buy in that direction for next day, if the pct change is greater than 1.5 in any direction we will buy in opposite direction for next day 
+# here 1.5 is SWITCH VALUE
+# if adj close gain from PREVIOUS DAY is between 0 - 1.5% or -1.5-0 we wiill buy in that direction for next day, if the pct change is greater than 1.5 in any direction we will buy in opposite direction for next day 
 
 # starting at index ---> see previous was -3.03683 we buy tqqq and multiply our previous val at index 1 which is 10000 by percent change ie 0.0765 so 10000 * 1.0765
 
@@ -57,6 +57,10 @@ switch_threshold = [x/2 for x in range(1,6)]
 
 balance_trend_qqq = [10000, 10000]
 qqq_strategy_trend = qqq_strategy(balance_trend_qqq, df_final, date_index)
+
+initial_investment = 10000
+qqq_data['Daily_Return'] = qqq_data['Adj Close'].pct_change()
+qqq_data['Investment_Value'] = initial_investment * (1 + qqq_data['Daily_Return']).cumprod()
 
 
 plt.figure(figsize=(18,9))
@@ -80,10 +84,11 @@ for i,j in enumerate(column_name):
 for i in column_name:
     plt.plot(df_final['date'], df_strategy_output[i], label=i)
 
+print(df_tqqq)
 
 plt.plot(df_final['date'], qqq_strategy_trend, linestyle='--', label="QQQ Intra Strategy")
-
-
+plt.plot(qqq_data.index, qqq_data['Investment_Value'], label='QQQ Investment')
+plt.axhline(y=10000, color='red', linestyle='--', label='Initial Investment')
 plt.title('TQQQ, SQQQ Trading Strategy')
 plt.xlabel('Date')
 plt.ylabel('Account Balance in Dollars')
